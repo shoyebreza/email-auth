be362ce3bb882be1bbf7b2280b053dd781d80505
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-09-19 10:02:03 +0600

    route path updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,44 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
 import {
   createBrowserRouter,
   RouterProvider,
-} from "react-router";
+} from "react-router-dom";
 import Main from './layout/Main.jsx';
+import Home from './components/Home/Home.jsx';
+import Login from './components/Login/Login.jsx';
+import Register from './components/Register/Register.jsx';
+import SignUp from './components/SignUp/SignUp.jsx';
 
-let router = createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      }, 
+      {
+        path: 'register',
+        element: <Register></Register>
+      },
+      {
+        path: 'signUp',
+        element: <SignUp></SignUp>
+      }
+    ]
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />,
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 )
